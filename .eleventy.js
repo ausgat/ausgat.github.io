6338638a6b140dc87acb29f721f5f494c8b75b39
Austin Gatchell <ausgat95@gmail.com>
2023-08-21 12:12:19 -0500

    Mobile style tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addWatchTarget("style.css");
   eleventyConfig.addWatchTarget("style-dark.css");
+  eleventyConfig.addWatchTarget("mobile.css");
   eleventyConfig.addPassthroughCopy("script.js");
   eleventyConfig.addWatchTarget("script.js");
   eleventyConfig.addPassthroughCopy("me.jpeg");
