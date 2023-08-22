e9ad046ed6ca0ef3211de2d662bfdad0c6d45a90
ausgat <ausgat@users.noreply.github.com>
2023-08-22 15:50:37 +0000

    deploy: 828fc6eeebc4b54693b970c4f622665d3f1c55c0

---
+++ js/random_tagline.js
@@ -0,0 +1,8 @@
+const hour = new Date().getHours()
+const quotes = [
+    "this is where i keep all my stuff",
+    "i hope you enjoy your stay",
+    "this blog ain't much, but it's mine",
+    hour >= 4 && hour < 20 ? "have a great day" : "have a good night",
+    "i think you're pretty cool"
+];
--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 function setDarkTheme() {
   var lightSwitch = document.getElementById("light-switch");
   lightSwitch.className = "right";
-  document.body.className = "dark";
+  document.body.className = "";
   setModeCookie("dark");
 }
 
 function setLightTheme() {
   var lightSwitch = document.getElementById("light-switch");
   lightSwitch.className = "left";
-  document.body.className = "";
+  document.body.className = "light";
   setModeCookie("light");
 }
 
 function switchTheme() {
+  var subtitle = document.getElementsByClassName("subtitle")[0];
   var lightSwitch = document.getElementById("light-switch");
   if (lightSwitch.className === "left") {
     setDarkTheme();
+    subtitle.textContent = "ah much better";
   } else if (lightSwitch.className === "right") {
     setLightTheme();
+    subtitle.textContent = "take breaks to avoid eye strain";
   }
 }
 
@@ -45,19 +48,27 @@ window.onload = function () {
   } else {
     if (
       window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
+      !window.matchMedia("(prefers-color-scheme: dark)").matches
     ) {
-      setDarkTheme();
+      setLightTheme();
     }
   }
   window
     .matchMedia("(prefers-color-scheme: dark)")
     .addEventListener("change", (event) => {
-      var lightSwitch = document.getElementById("light-switch");
-      if (event.matches) {
-        setDarkTheme();
-      } else {
-        setLightTheme();
+      if (mode === "") {
+        var lightSwitch = document.getElementById("light-switch");
+        if (event.matches) {
+          setDarkTheme();
+        } else {
+          setLightTheme();
+        }
       }
     });
+    
+    var subtitle = document.getElementsByClassName("subtitle")[0];
+    if (subtitle) {
+        var random_n = (Math.floor(Math.random() * quotes.length));
+        subtitle.textContent = quotes[random_n];
+    }
 };
