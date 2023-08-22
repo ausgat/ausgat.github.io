b9c2f5260a1324310fdfe878fad86226c06e8ce9
Austin Gatchell <ausgat95@gmail.com>
2023-08-21 20:41:13 -0500

    Change a tagline

--- js/random_tagline.js
+++ js/random_tagline.js
@@ -3,6 +3,6 @@ const quotes = [
     "this is where i keep all my stuff",
     "i hope you enjoy your stay",
     "this blog ain't much, but it's mine",
-    hour >= 4 && hour <= 20 ? "have a great day" : "have a good night",
-    "you're a good person"
+    hour >= 4 && hour < 20 ? "have a great day" : "have a good night",
+    "i think you're pretty cool"
 ];
